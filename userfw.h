7fc2b30bc4ecbd797ef85ccfa5b6b07331cffc61
yegemberdin <yegemberdin@gmail.com>
2019-03-13 05:26:11 +0600

    headers for firewall

---
+++ userfw.h
@@ -0,0 +1,15 @@
+
+/*headers*/
+#include <linux/types.h>
+
+struct mf_rule_struct {
+    unsigned int src_ip;
+    unsigned int dest_ip;
+    unsigned int src_port;
+    unsigned int dest_port;
+    unsigned int in;
+    unsigned int proto;
+    char action;  //NONE=0; ADD=1; REMOVE=2; VIEW=3
+}
+
+
